cfb490f992d6c225ba64687b24e4a71c8ee44b1e
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-10-25 22:48:22 -0700

    Project with Sign Up and QR Code Views for users to sing up or enter a QR code. Working for when entering those windows from the walkthrough.

--- SpartanSurvey/ViewController.swift
+++ SpartanSurvey/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 //  GitHub commit/push test
 //  Verify submission is succesful
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, BWWalkthroughViewControllerDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showWalkThrough() {
+        print("walkthrough button pressed")
+        
+        //  Instantiate main storyboard for providing viewing priviledge to the walkthrough 
+        let stb = UIStoryboard(name: "Main", bundle: nil)
+        
+        //  Build the walkthrough
+        let walkthrough = stb.instantiateViewController(withIdentifier: "walk0") as! BWWalkthroughViewController
+        let page_one = stb.instantiateViewController(withIdentifier: "walk1") as UIViewController
+        let page_two = stb.instantiateViewController(withIdentifier: "walk2") as UIViewController
+        let page_three = stb.instantiateViewController(withIdentifier: "walk3") as UIViewController
+        
+        //  Attach pages to master
+        walkthrough.delegate = self
+        walkthrough.addViewController(page_one)
+        walkthrough.addViewController(page_two)
+        walkthrough.addViewController(page_three)
+        
+        self.present(walkthrough, animated: true, completion: nil)
+    }
+    
+    func walkthroughCloseButtonPressed() {
+        self.dismiss(animated: true, completion: nil)
+    }
 
 }
 
