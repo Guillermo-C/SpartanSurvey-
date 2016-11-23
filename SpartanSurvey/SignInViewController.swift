032f59e5b4153812de0c84c879ffdef71f297d7d
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-11-23 14:34:22 -0800

    Update: added a working popover, to guide the user, for  the SignInViewController.

--- SpartanSurvey/SignInViewController.swift
+++ SpartanSurvey/SignInViewController.swift
@@ -13,7 +13,7 @@ import UIKit
     @objc optional func signInBackButtonPressed()
 }
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UIPopoverPresentationControllerDelegate {
 
     var delegate: SignInViewControllerDelegate?
     
@@ -36,14 +36,26 @@ class SignInViewController: UIViewController {
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "signInInfoSegue"{
+            
+            let viewC = segue.destination
+            let controller = viewC.popoverPresentationController
+            
+            if (controller != nil) {
+                controller?.delegate = self
+            }
+            
+        }
     }
-    */
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
 
 }
