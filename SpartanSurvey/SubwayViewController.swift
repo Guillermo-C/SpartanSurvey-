59d0c7128f80743432e098c199f0eebd55db3d35
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-11-23 19:35:42 -0800

    Filled all pickers with appropriate data for the Subway survey.

--- SpartanSurvey/SubwayViewController.swift
+++ SpartanSurvey/SubwayViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SubwayViewController: UIViewController {
+class SubwayViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,59 @@ class SubwayViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var subOfDayPicker: UIPickerView!
+    @IBOutlet weak var breakFastPicker: UIPickerView!
+    @IBOutlet weak var saladPicker: UIPickerView!
+    @IBOutlet weak var extrasPicker: UIPickerView!
     
-
+    var subOfDayTypes = ["Sweet Onion Chicken Teriyaki","Oven Roasted Chicken","Turkey Breast","Classic Tuna","Black Forest Ham"]
+    
+    var breakFastTypes = ["Beacon, Egg & Cheese","Egg & Cheese","Steak, Egg & Cheese"]
+    
+    var saladTypes = ["Corned Beef Reuben Salad","Turkey Reuben Salad","Spicy Italian Salad","Veggie Delite Salad","Tuna Salad","Subway Club Salad"]
+    
+    var extrasTypes = ["Pepperoni","Cheese","Bacon"]
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if (pickerView == breakFastPicker){
+            return breakFastTypes[row]
+        }
+        
+        if (pickerView == saladPicker){
+            return saladTypes[row]
+        }
+        
+        if (pickerView == extrasPicker){
+            return extrasTypes[row]
+        }
+        
+        
+        return subOfDayTypes[row]
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        if (pickerView == breakFastPicker){
+            return breakFastTypes.count
+        }
+        
+        if (pickerView == saladPicker){
+            return saladTypes.count
+        }
+        
+        if (pickerView == extrasPicker){
+            return extrasTypes.count
+        }
+        
+        return subOfDayTypes.count
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        
+        return 1
+    }
+    
     /*
     // MARK: - Navigation
 
