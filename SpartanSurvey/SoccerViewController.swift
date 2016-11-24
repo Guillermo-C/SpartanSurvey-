f72931203a522638695ac4a5211c8d0bc0edcefc
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-11-23 20:31:51 -0800

    Finished the Soccer survey with corresponding questions and possible answers (pickers and textfield). The Soccer survey needs work on the constraints.

--- SpartanSurvey/SoccerViewController.swift
+++ SpartanSurvey/SoccerViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class SoccerViewController: UIViewController {
+class SoccerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +22,20 @@ class SoccerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var soccerPlayerPicker: UIPickerView!
+    @IBOutlet weak var soccerTeamPicker: UIPickerView!
+    @IBOutlet weak var nationalTeamPicker: UIPickerView!
+    @IBOutlet weak var desiredPositionPicker: UIPickerView!
+    
+    var soccerPlayerVals = ["Cristiano Ronaldo","Lionel Messi","Gareth Bale","Manuel Neuer","Wayne Rooney","Iker Casillas"]
+    
+    var teamVals = ["Real Madrid C.F.","Inter Milan","Atlético Madrid","FC Barcelona","Arsenal F.C."]
+    
+    var nationalTeamVals = ["Japan National Team","USA National Team","Spain National Team","Argentina National Team"]
+    
+    var soccerPositionVals = ["goalkeeper","defensive midfielder","forward","striker"]
+    
+    
     /*
     // MARK: - Navigation
 
@@ -31,5 +45,47 @@ class SoccerViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if (pickerView == soccerPlayerPicker){
+            return soccerPlayerVals[row]
+        }
+        
+        if (pickerView == soccerTeamPicker){
+            return teamVals[row]
+        }
+        
+        if (pickerView == nationalTeamPicker){
+            return nationalTeamVals[row]
+        }
+        
+        
+        return soccerPositionVals[row]
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        
+        if (pickerView == soccerPlayerPicker){
+            return soccerPlayerVals.count
+        }
+        
+        if (pickerView == soccerTeamPicker){
+            return teamVals.count
+        }
+        
+        if (pickerView == nationalTeamPicker){
+            return nationalTeamVals.count
+        }
+        
+        
+        return soccerPositionVals.count
+
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        
+        return 1
+    }
 
 }
