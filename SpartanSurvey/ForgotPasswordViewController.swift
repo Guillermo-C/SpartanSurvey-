83d7ed15fac0020374d9f84c56a1a35e5231f23b
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-11-27 15:38:30 -0800

    Added an alert that request input from the user for when changing password. More testing needed.

--- SpartanSurvey/ForgotPasswordViewController.swift
+++ SpartanSurvey/ForgotPasswordViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class ForgotPasswordViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+    
+    //  for testing only
+    //  Alert for wrong credentials
+    let secQuesAlert = UIAlertController(title: "Sec Question", message: "The actual answer would come here...", preferredStyle: UIAlertControllerStyle.alert)
+    //  end for testing
+    
+    
+    var passwordTextField: UITextField?
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        secQuesAlert.addTextField{(passwordTextField) in
+            passwordTextField.text = "Testing"
+        }
+        
+        /*secQuesAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = secQuesAlert.passwordTextField![0] // Force unwrapping because we know it exists.
+            print("Text field: \(passwordTextField.text)")
+        }))*/
+        
+        secQuesAlert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { (action: UIAlertAction!) in
+            print("Handle Ok logic here")
+            let textField = self.secQuesAlert.textFields![0] // Force unwrapping because we know it exists.
+            print("Text field: \(textField.text)")
+            
+        }))
+        
 
         // Do any additional setup after loading the view.
     }
