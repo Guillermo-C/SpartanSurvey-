1f3233f81120f16d89938fecbd72ebe9ba123a57
Guillermo Colin <ninjamemo@guillermos-macbook-pro.local>
2016-12-08 16:37:22 -0800

    Cleaned up the QRCodeViewController.swift file.

--- SpartanSurvey/QRCodeViewController.swift
+++ SpartanSurvey/QRCodeViewController.swift
@@ -8,21 +8,29 @@
 
 import UIKit
 
+//  Create an obj protcol for using the back button within the walkthrough, .
 @objc protocol QRCodeViewControllerDelegate{
     @objc optional func QRBackButtonPressed()
 }
 
+//  This class is for the QR Code. The QR Code is simulated with a string, which is: '1A3BZ99'.
 class QRCodeViewController: UIViewController, UIPopoverPresentationControllerDelegate {
     
+    //  Delegate var for the QR Code class which is used in the walkthrough.
     var delegate: QRCodeViewControllerDelegate?
     
     //  qrCode needed to be taken to the appropriate survey. 
     var qrCode:String = "1A3BZ99"
     
+    //  UITextField which takes the qrCode String value.
     @IBOutlet weak var qRCodeEntry: UITextField!
     
-    let wrongQRAlert = UIAlertController(title: "Wrong QR Code", message: "Please verify your QR code.", preferredStyle: UIAlertControllerStyle.alert)
-
+    //  Back button for the bar menu in this QRCodeViewController.
+    @IBAction func QRBackButtonPressed(_ sender: UIBarButtonItem) {
+        
+        delegate?.QRBackButtonPressed!()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,34 +38,36 @@ class QRCodeViewController: UIViewController, UIPopoverPresentationControllerDel
         }))
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func QRBackButtonPressed(_ sender: UIBarButtonItem) {
-        
-        delegate?.QRBackButtonPressed!()
-    }
+    //  Information button which displays a PopOver for additional information when the user requests it.
     @IBAction func showInfo(_ sender: UIButton) {
         self.performSegue(withIdentifier: "qRInfoSegue", sender: self)
     }
     
-    
-    //  For testing only
-    
-    var cloud = CloudKitEngine()
-    @IBAction func submit(_ sender: UIButton) {
-        let userQREntry = qRCodeEntry.text!
+    //  Alert user in case of an incorrect QR code.
+    let wrongQRAlert = UIAlertController(title: "Wrong QR Code", message: "Please verify your QR code.", preferredStyle: UIAlertControllerStyle.alert)
 
+    
+
+
+    //  func to validate the QR Code submitted by the user. And if it's valid then show the Yogurtland survey.
+    @IBAction func submit(_ sender: UIButton) {
+        
+        let userQREntry = qRCodeEntry.text!
         validateQRCode(userEntry: userQREntry, qrCode: qrCode, targetVC: self, alert: wrongQRAlert)
+        
     }
 
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation.
+    //  func prepare() is used for the segue 'qRInfoSegue'
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
@@ -75,6 +85,7 @@ class QRCodeViewController: UIViewController, UIPopoverPresentationControllerDel
             
     }
 
+    //  func required for the PopOver.
     func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
         return .none
     }
@@ -89,8 +100,7 @@ class QRCodeViewController: UIViewController, UIPopoverPresentationControllerDel
         else{
             targetVC.present(alert, animated: true, completion: nil)
         }
-    }
-    
+    }    
     
 
 }
